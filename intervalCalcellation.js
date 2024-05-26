fc87cb4c8d3b4ff55c8065d5b73b827c99dd3d4d
Sazzad Hossain <sazzad@nextgenitltd.com>
2024-05-26 14:50:45 +0600

    interval calcellation

---
+++ intervalCalcellation.js
@@ -0,0 +1,32 @@
+var cancellable = function (fn, args, t) {
+  const results = [];
+  let startTime = Date.now();
+
+  const recordExecution = () => {
+    let currentTime = Date.now() - startTime;
+    let result = fn(...args);
+    results.push({ time: currentTime, returned: result });
+  };
+
+  recordExecution();
+
+  let timer = setInterval(recordExecution, t);
+
+  let cancelFn = () => {
+    clearInterval(timer);
+    return results;
+  };
+
+  return cancelFn;
+};
+
+const fn = (x) => x * 2;
+const args = [4];
+const t = 35;
+
+const cancel = cancellable(fn, args, t);
+
+setTimeout(() => {
+  const results = cancel();
+  console.log(results);
+}, 200);
